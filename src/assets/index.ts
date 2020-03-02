8cfa72370f6384b338e6750d235fa5b58759e425
Ali Zahid <ali.zahid@live.com>
2020-03-02 11:16:56 +0400

    Header updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 import img_close_window from './img/close_window.svg'
+import img_help_window from './img/help_window.svg'
 import img_minimize_window from './img/minimize_window.svg'
 
-export { img_close_window, img_minimize_window }
+export { img_close_window, img_help_window, img_minimize_window }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,35 +3,50 @@ import './header.scss'
 import { remote } from 'electron'
 import { FunctionalComponent, h } from 'preact'
 
-import { img_close_window, img_minimize_window } from '../assets'
+import {
+  img_close_window,
+  img_help_window,
+  img_minimize_window
+} from '../assets'
 
 export const Header: FunctionalComponent = () => (
   <header>
     <h1>Dota Notifier</h1>
     <nav>
-      <a>
-        <img
-          alt="Minimize"
-          onClick={() => remote.getCurrentWindow().minimize()}
-          src={img_minimize_window}
-        />
-      </a>
-      <a>
-        <img
-          alt="Close"
-          onClick={async () => {
-            const { response } = await remote.dialog.showMessageBox({
-              buttons: ['Yes', 'No'],
-              message: 'Are you sure you want to quit?',
-              title: 'Dota Notifier'
-            })
+      <a
+        href="#help"
+        onClick={event => {
+          event.preventDefault()
 
-            if (response === 0) {
-              remote.app.quit()
-            }
-          }}
-          src={img_close_window}
-        />
+          remote.shell.openExternal('https://dota-notifier.now.sh')
+        }}>
+        <img alt="Help" src={img_help_window} />
+      </a>
+      <a
+        href="#minimize"
+        onClick={event => {
+          event.preventDefault()
+
+          remote.getCurrentWindow().minimize()
+        }}>
+        <img alt="Minimize" src={img_minimize_window} />
+      </a>
+      <a
+        href="#close"
+        onClick={async event => {
+          event.preventDefault()
+
+          const { response } = await remote.dialog.showMessageBox({
+            buttons: ['Yes', 'No'],
+            message: 'Are you sure you want to quit?',
+            title: 'Dota Notifier'
+          })
+
+          if (response === 0) {
+            remote.app.quit()
+          }
+        }}>
+        <img alt="Close" src={img_close_window} />
       </a>
     </nav>
   </header>
